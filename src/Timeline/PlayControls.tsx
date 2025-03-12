520ac4f1aa0c9fe290e4d0020f55df59d5479a49
Boxi <boxi800108@gmail.com>
2025-03-12 20:53:22 +0800

    feat: implement PlayControls

--- src/Timeline/PlayControls.tsx
+++ src/Timeline/PlayControls.tsx
@@ -1,4 +1,9 @@
+import { useCallback, useEffect } from 'react';
 import NumberInput from '../components/NumberInput';
+
+export const MIN_DURATION = 100;
+export const MAX_DURATION = 6000;
+export const STEP = 10;
 
 type PlayControlsProps = {
   time: number;
@@ -8,29 +13,58 @@ type PlayControlsProps = {
 };
 
 export const PlayControls = ({ time, setTime, duration, setDuration }: PlayControlsProps) => {
+  const timeFormat = useCallback((newTime: number) => {
+    let value = Math.max(0, Math.round(newTime / STEP) * STEP); // Round to nearest multiple of 10
+    if (newTime > duration) {
+      value = duration;
+    }
+    return value;
+  }, [duration]);
+
+  const durationFormat = useCallback((newDuration: number) => {
+    const value = Math.max(MIN_DURATION, Math.min(MAX_DURATION, Math.round(newDuration / STEP) * STEP)); // Clamp duration and round
+    return value;
+  }, [])
+
+
+  const handleTimeChange = (newTime: number) => {
+    setTime(newTime);
+  };
+
+  const handleDurationChange = (newDuration: number) => {
+    setDuration(newDuration);
+  };
+
+  useEffect(() => {
+    if (time > duration) {
+      setTime(duration);
+    }
+  }, [time, duration]);
+
   return (
     <div
-      className="flex items-center justify-between border-b border-r border-solid border-gray-700 
- px-2"
+      className="flex items-center justify-between border-b border-r border-solid border-gray-700 px-2"
       data-testid="play-controls"
     >
       <NumberInput
         value={time}
-        onChange={setTime}
+        onChange={handleTimeChange}
+        format={timeFormat}
         dataTestId="current-time-input"
         min={0}
-        max={duration}
-        step={10}
+        max={duration} // Ensure max is dynamic based on duration
+        step={STEP}
         label="Current"
       />
       -
       <NumberInput
         value={duration}
-        onChange={setDuration}
+        onChange={handleDurationChange}
+        format={durationFormat}
         dataTestId="duration-input"
-        min={100}
-        max={6000}
-        step={10}
+        min={MIN_DURATION}
+        max={MAX_DURATION}
+        step={STEP}
         label="Duration"
       />
     </div>
